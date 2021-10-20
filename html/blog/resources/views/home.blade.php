56e5cb296c16fe6fce8b2119bf39f9c2f0ac4df7
Ryan Kruberg <rkruberg@gmail.com>
2021-10-20 15:26:41 +0300

    format changes

--- html/blog/resources/views/home.blade.php
+++ html/blog/resources/views/home.blade.php
@@ -3,25 +3,25 @@
 @section('title', 'Home')
 
 @section('content')
-    <form action="/post" method="POST">
-        @csrf
-        <div class="mt-3">
-            <textarea class="form-control" placeholder="Your thoughts" name="content"></textarea>
-        </div>
-        <div class="mt-3">
-            <input type="submit" class="btn btn-primary" value="Post">
-        </div>
-    </form>
-    @foreach($posts as $post)
-        <div class="card mt-3 custom">
-            <div class="card-body">
-                <p>{{ $post->content }}</p>
-                <a href="/post/{{$post->id}}">read more</a>
-            </div>
-            <div class="card-footer text-muted">
-                <p>Posted by - {{\App\Models\User::find($post->user_id )->name}} ({{$post->created_at->diffForHumans()}})</p>
-            </div>
-        </div>
-    @endforeach
+<form action="/post" method="POST">
+    @csrf
+    <div class="mt-3">
+        <textarea class="form-control" placeholder="Your thoughts" name="content"></textarea>
+    </div>
+    <div class="mt-3">
+        <input type="submit" class="btn btn-primary" value="Post">
+    </div>
+</form>
+@foreach($posts as $post)
+<div class="card mt-3 custom">
+    <div class="card-body">
+        <p>{{ $post->content }}</p>
+        <a href="/post/{{$post->id}}">read more</a>
+    </div>
+    <div class="card-footer text-muted">
+        <p>Posted by - {{\App\Models\User::find($post->user_id )->name}} ({{$post->created_at->diffForHumans()}})</p>
+    </div>
+</div>
+@endforeach
 
 @endsection
--- html/blog/resources/views/post.blade.php
+++ html/blog/resources/views/post.blade.php
@@ -4,20 +4,19 @@
 
 @section('content')
 
-        <div class="card mt-3">
-            <div class="card-body">
-                {{ $post->content }}
-            </div>
-            <div class="card-footer text-muted">
-                <p>Posted By - {{\App\Models\User::find($post->user_id )->name}} ({{$post->created_at->diffForHumans()}})</p>
-            </div>
-        </div>
-        @foreach ($post->comments as $comment)
-            <div class="alert alert-dark mt-2">
-                {{$comment->content}}
-                <p></p>
-                <p>Comment from - {{\App\Models\User::find($comment->user_id )->name}}</p>
-            </div>
-        @endforeach
+<div class="card mt-3">
+    <div class="card-body">
+        {{ $post->content }}
+    </div>
+    <div class="card-footer text-muted">
+        <p>Posted By - {{\App\Models\User::find($post->user_id )->name}} ({{$post->created_at->diffForHumans()}})</p>
+    </div>
+</div>
+@foreach ($post->comments as $comment)
+<div class="alert alert-dark mt-2">
+    {{$comment->content}}
+    <p></p>
+    <p>Comment from - {{\App\Models\User::find($comment->user_id )->name}}</p>
+</div>
+@endforeach
 @endsection
-
